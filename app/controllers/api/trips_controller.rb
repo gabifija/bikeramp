ec2ea9e7e290dbc23595da9f704c3af721c6b150
Gabi <gabrielafijalkowska@gmail.com>
2018-02-09 19:36:49 +0100

    Updated all endpoints for trips

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -8,20 +8,34 @@ class API::TripsController < ApplicationController
     render json: @trips
   end
 
-  # GET /trips/1
+  # GET /api/trips/1
   def show
     render json: @trip
   end
 
-  # POST /trips
+  # POST /api/trips
   def create
     @trip = Trip.new(trip_params)
 
     if @trip.save
-      render json: @trip, status: :created, location: @trip
+      render json: @trip, status: :created, location: api_trip_path(@trip.id)
     else
       render json: @trip.errors, status: :unprocessable_entity
     end
+  end
+
+  # PATCH/PUT /api/trips/1
+  def update
+    if @trip.update(trip_params)
+      render json: @trip
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/trips/1
+  def destroy
+    @trip.destroy
   end
 
   private
@@ -32,6 +46,6 @@ class API::TripsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def trip_params
-      params.require(:trip).permit(:start_address, :destination_address, :price, :date)
+      params.permit(:start_address, :destination_address, :price, :date)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resources :trips, only: [:index, :create] do
+    resources :trips do
         get 'stats/weekly', on: :collection, controller: "stats", action: "get_weekly_stats"
         get 'stats/monthly', on: :collection, controller: "stats", action: "get_monthly_stats"
     end
