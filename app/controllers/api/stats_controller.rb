c150f647b62cc54ca6210e9a47cdcb42fc906ac6
Gabi <gabrielafijalkowska@gmail.com>
2018-02-12 08:42:52 +0100

    Refactored trip model for monthly stats

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -23,28 +23,11 @@ class API::StatsController < ApplicationController
     @dates.each do |date|
       data << {
         day: Trip.convert_date(date),
-        total_distance: count_total_distance(date),
-        avg_ride: count_average_ride(date),
-        avg_price: count_average_price(date)
+        total_distance: Trip.count_total_distance(date),
+        avg_ride: Trip.count_average_ride(date),
+        avg_price: Trip.count_average_price(date)
       }
     end
     json_response(data)
   end
-
-  private
-
-    def count_total_distance(date)
-      distance = Trip.where(date: date).sum(:distance)
-      ActionController::Base.helpers.number_to_human(distance, units: {unit: "km"})
-    end
-
-    def count_average_ride(date)
-      ride = Trip.where(date: date).average(:distance)
-      ActionController::Base.helpers.number_to_human(ride, units: {unit: "km"})
-    end
-
-    def count_average_price(date)
-      price = Trip.where(date: date).average(:price)
-      ActionController::Base.helpers.number_to_human(price, units: {unit: "PLN"})
-    end
 end
